716254705b660ca5a0518dcc810727e4a2d746ca
Iqbal Sandhu <smartsingh@gmail.com>
2021-08-04 01:07:58 -0700

    Update logic1.js
    Implementing tile size fixes and street map fix

--- logic1.js
+++ logic1.js
@@ -54,34 +54,42 @@ function createMap(earthquakes) {
  
   var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
     maxZoom: 18,
+    zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   });
 
   var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
     maxZoom: 18,
+    zoomOffset: -1,
     id: "mapbox/satellite-v9",
     accessToken: API_KEY
   });
 
   var outdoorMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
     maxZoom: 18,
+    zoomOffset: -1,
     id: "mapbox/outdoors-v11",
     accessToken: API_KEY
   });
 
   var navMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
     maxZoom: 18,
+    zoomOffset: -1,
     id: "mapbox/navigation-day-v1",
     accessToken: API_KEY
   });
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
-    "Satellite Map": streetMap,
+    "Street Map": streetMap,
     "Satellite Map": satelliteMap,
     "Outdoors Map": outdoorMap,
     "Navigate Map": navMap
